5050b718b0c0795796d10e1340f65c63bbcad445
Wyeth Jackson <wyethjackson@Wyeths-MacBook-Pro.local>
2022-05-15 10:00:11 -0400

    Build: graphql/server foundation

---
+++ src/main/java/com/jacksonproductions/server/BaseController.java
@@ -0,0 +1,19 @@
+package com.jacksonproductions.server;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//
+//@RestController
+//public class BaseController {
+//    @GetMapping("/")
+//	public String index() {
+//		return "OH, HELLO";
+//	}
+//
+////	@GetMapping("/graphql")
+////	@CrossOrigin(origins = "http://localhost:3000")
+////	public String graphQL() {
+////		return "OH, HELLO GRAPHQL!";
+////	}
+//}
---
+++ src/test/java/com/jacksonproductions/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonproductions.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
